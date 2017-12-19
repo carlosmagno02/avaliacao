5d4bd2632855de24e823b0722a406c4f7c91038f
root <willianpilar@hotmail.com>
2017-12-19 07:58:11 -0200

    foi

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 read -p 'Pressione [enter] para instalar o dialog ou ctrl+c para nao instalar'
-echo 'instalando o dialog' &
+echo 'instalando o dialog' 
 apt install dialog -y
 mkdir -p /usr/share/avaliação
 cp ./.avaliação/* /usr/share/avaliação
-ln -s /usr/share/avaliação/menu.sh /usr/bin/avaliação
+ln -s /usr/share/avaliação/Avaliação\ 360.sh /usr/bin/avaliação
