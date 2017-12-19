e46b63731bc33b28f9a7623477efee4f73f4cb12
root <willianpilar@hotmail.com>
2017-12-19 07:51:45 -0200

    foi

--- .avaliação/vitor.sh
+++ .avaliação/vitor.sh
@@ -4,7 +4,7 @@ dialog --stdout \
 --title 'Vitor' \
 --msgbox 'Positivo: ajudou nas pesquisas e no desenvolvimento do programa.
 Negativo: faltou muitas vezes, nao se comprometeu com o projeto e esteve muito disperso no dias em que esteve presente.
-nota: 20
+nota: 30
 ' \
 0 0
 . /usr/share/avaliação/Avaliação\ 360.sh
